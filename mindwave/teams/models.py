92fb12b64d20d037fd217cd6df8c470d83b1a764
VP859 <mierzwa.jan.6@gmail.com>
2024-09-28 12:31:59 +0200

    created teams

---
+++ mindwave/teams/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Team(models.Model):
+    team_name = models.CharField(max_length=100)
+    members = models.ForeignKey('accounts.Profile', on_delete=models.CASCADE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.team_name
